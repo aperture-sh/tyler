917bc4d4be084dc61235d2611509ad9a43508901
Florian Zouhar <florian.zouhar@igd.fraunhofer.de>
2018-09-05 16:19:09 +0200

    unitque id method for tiles

--- src/models.kt
+++ src/models.kt
@@ -10,3 +10,5 @@ data class Geometry(val type: String, var coordinates: MutableList<List<List<Dou
 data class Tile(val featureCollection: FeatureCollection, val z: Int, val x: Int, val y: Int, val extend: Int)
 
 typealias BoundingBox = Pair<Pair<Double, Double>, Pair<Double, Double>>
+
+fun toID(z: Int, x: Int, y: Int) = (((1 shl z) * y + x) * 32) + z;
